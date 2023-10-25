1a5d5762792770243d7658cfceee8252ffef6fd4
Liao <liao.yiqiang@qq.com>
2023-10-25 15:49:47 +0000

    add another test

--- tests/components/translate_conversation/test_agent.py
+++ tests/components/translate_conversation/test_agent.py
@@ -10,3 +10,10 @@ class input_class:
 def test_language_detect():
     a = TranslateConversationAgent(skip_rwkv=True, skip_translate=True)
     assert a.use_language_detect(input_class("", "vad är ditt namn")) == "sv"
+
+
+def test_support_language():
+    a = TranslateConversationAgent(
+        skip_rwkv=True, skip_translate=True, skip_language_detect=True
+    )
+    assert a.supported_languages == "*"
